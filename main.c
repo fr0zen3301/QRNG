548b4061f1df47942cb7e1a93dd3dcdf509f359e
fr0zen3301 <rikudosanin@icloud.com>
2024-09-29 22:00:13 +0900

    V1.0.1

--- main.c
+++ main.c
@@ -6,24 +6,37 @@
 int quantum_superposition() {
     
     // Simulate a 50% probability of collapsins to 0 or 1(polarization)
-    
+    // It's being detected vertically or horizontally (0 or 1)
     // Collapse of the wave function
     return rand() % 2; 
 }
 
-void quantum_random_numbers(int n) {
+
+void save_to_file(int n, const char* filename) {
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Error opening file\n");
+        return;
+    }
     printf("Quantum Random Numbers: ");
+    // Generate and write the random bits
     for (int i = 0; i < n; i++) {
-        printf("%d", quantum_superposition());
+        int bit = quantum_superposition();
+        printf("%d", bit);
+        fprintf(file, "%d", bit); // Write the bit (0 or 1)
     }
     printf("\n");
+    // Close the file
+    fclose(file);
+    printf("Random numbers saved to %s\n", filename);
 }
 
 int main() {
+    char* random_number = "quantum_random_number.txt";
     int num_bits; // Ask for the amount of random bits
     printf("Number of random bits to generate: ");
-    scanf("%d", &num_bits); 
-    srand(time(0));
-    quantum_random_numbers(num_bits);
+    scanf("%d", &num_bits);
+    srand(time(0)); // To not get the same sequence every time
+    save_to_file(num_bits, random_number);
     return 0;
 }
