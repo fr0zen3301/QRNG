1fac68fdf6cdcf07bcb694b99fea7b1c15befe69
fr0zen3301 <rikudosanin@icloud.com>
2024-10-14 13:26:55 +0900

    V1.1 ~ V2.0 calling C func from Python.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import qrng_wrapper
 
-n_bits = 256
+n_bits = int(input("Number of bits to generate: "))
 filename = "qrn.txt"
 qrng_wrapper.seed_rng() # seed the random number generator
 qrng_wrapper.generate_qrng_bytes(n_bits, filename)
